84f5ad4b27160198c0bf35ce353da1333a3b06ee
Flash-at-DMV <wangjin8855@hotmail.com>
2020-04-14 01:25:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+def is_on_list(x_list, x):
+  return x in x_list
+
+def get_x(x_list, index):
+  return x_list[index]
+
+def add_x(x_list, x):
+  x_list.append(x)
+
+def remove_x(x_list, x):
+  x_list.remove(x)
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+
+
+
+
